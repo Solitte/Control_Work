7848911507fa5e5766daac802cbc74a94b799a09
SolitE <solitte@yandex.ru>
2022-10-01 02:06:24 +0500

    PrintArray

--- Program.cs
+++ Program.cs
@@ -14,3 +14,12 @@ string[] InputArray()
     }
     return array;
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();    
+}
