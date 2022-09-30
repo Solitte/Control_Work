0b69df52259768aa6e91ae3739263e8f0c63ba3e
SolitE <solitte@yandex.ru>
2022-10-01 02:03:25 +0500

    InputArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+string[] InputArray()
+{
+    Console.WriteLine("Input size of the array");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    Console.WriteLine($"Input elements of array: ");
+    for (int i = 0; i<size; i++)
+    {
+        Console.Write($"{i} - ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
